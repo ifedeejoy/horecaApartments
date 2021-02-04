5548da12af9f9379eb4a525c120b615956ef00be
Goozi <goozionline@gmail.com>
2021-02-04 11:58:17 +0100

    updated user status for owners section

--- public/app-assets/js/scripts/pages/app-owner-list.js
+++ public/app-assets/js/scripts/pages/app-owner-list.js
@@ -14,9 +14,9 @@ $(function() {
         newUserSidebar = $('.new-user-modal'),
         newUserForm = $('.add-new-user'),
         statusObj = {
-            0: { title: 'Pending', class: 'badge-light-warning' },
-            2: { title: 'Active', class: 'badge-light-success' },
-            1: { title: 'Offline', class: 'badge-light-secondary' }
+            'pending': { title: 'Pending', class: 'badge-light-warning' },
+            'online': { title: 'Active', class: 'badge-light-success' },
+            'offline': { title: 'Offline', class: 'badge-light-secondary' }
         };
 
     var assetPath = '../../../app-assets/',
