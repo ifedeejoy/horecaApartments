31ad5b41eb431a772d8de9c286520afa907d89d2
Goozi <goozionline@gmail.com>
2021-04-24 13:55:55 +0100

    see you, you think say i no go catch that error

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -63,7 +63,6 @@ class EventController extends Controller
         $listEvents->all();
         // events
         $events = collect();
-        dd($listEvents);
         foreach($listEvents as $event):
             if($event->status == 'cancelled'):
                 $event->where('google_id', $event->id)->delete();
